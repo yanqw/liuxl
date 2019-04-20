9f358090ee63d22a9c4876fafe720c01fb4ec1ab
yanqw <271853976@qq.com>
2019-04-20 15:53:03 +0800

    Signed-off-by: yanqw <271853976@qq.com>

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;  
     var pwd=document.getElementById("pwd").value;
-    if(userName=="哈哈哈" &&  pwd=="123"){ 
+    if(userName=="刘小玲" &&  pwd=="0318"){ 
 		event.preventDefault();
 		$('form').fadeOut(500);
 		$('.wrapper').addClass('form-success');
 		setTimeout(function(){location.href="BirthdayCake.html";},2000);
 	}
 	else{
-		alert("Wrong Password");
+		alert("傻瓜，再好好想想呢😕");
 	}
 });
